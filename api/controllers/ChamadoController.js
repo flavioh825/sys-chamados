ea113f438ddb11b89c2915d469ace9f5db8bd47c
Flavio FD. Da Silva Henrique <flavio.henrique@icn.org.br>
2016-08-19 16:57:26 -0300

    Criação dos models Categoria e Chamado

---
+++ api/controllers/ChamadoController.js
@@ -0,0 +1,11 @@
+/**
+ * ChamadoController
+ *
+ * @description :: Server-side logic for managing chamadoes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Chamado.js
@@ -0,0 +1,30 @@
+/**
+ * Chamado.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+		ocorrencia: {
+			type: 'string',
+			required: true
+		},
+		descricao: {
+			type: 'text',
+			required: true
+		},
+		prioridade: {
+			type: 'string',
+			required: true
+		},
+		situacao: {
+			type: 'string',
+		}
+
+  }
+};
+
